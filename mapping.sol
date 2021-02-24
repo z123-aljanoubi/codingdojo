88d961a4e4c5729d1db744c967e424852cdbf967
z123-aljanoubi <78320109+z123-aljanoubi@users.noreply.github.com>
2021-02-24 23:13:48 +0300

    Create mapping.sol

---
+++ mapping.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.7.0 <0.8.0;
+
+contract MappingCites {
+
+    mapping(string => string ) cities;
+    
+    function add(string memory _city, string memory _country) public {
+        cities[_city] =  _country;
+        
+    }
+    
+    function get(string memory _name) public view returns(string memory) {
+        return cities[_name];
+    }
+    
+    function remove(string memory _nam) public {
+        delete(cities[_nam]);
+    }
+    
+    
+}
